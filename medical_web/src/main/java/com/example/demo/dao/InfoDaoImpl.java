5f20303ac059b5c9d1ff9a4b22527d8513cfb199
Ambition818 <52785075+Ambition818@users.noreply.github.com>
2021-05-29 04:38:57 +0800

    add doctor RegisterTime

--- medical_web/src/main/java/com/example/demo/dao/InfoDaoImpl.java
+++ medical_web/src/main/java/com/example/demo/dao/InfoDaoImpl.java
@@ -83,7 +83,8 @@ public class InfoDaoImpl implements InfoDao {
             			rs.getString("evaluate"),
             			rs.getString("email"),
             			rs.getString("tel"),
-            			rs.getString("vx")
+            			rs.getString("vx"),
+                        rs.getTime("registerTime")
     			
             			);
             	reports.add(re);
